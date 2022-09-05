e83b61d54472dd8621e353d9cd1a8be383d71ccd
unknown <ebranafas@gmail.com>
2022-09-05 13:29:53 +0430

    Change style for mobile size

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -10,33 +10,35 @@ function CardContainer() {
   return (
     <div className="bg-white text-dark w-[90%] md:w-[556px] rounded-xl mx-auto">
       <div className="p-7">
-        <div className="flex justify-between items-center">
-          <p className="opacity-70 uppercase">100k pageviews</p>
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <p className="opacity-70 uppercase pb-5 md:pb-0">100k pageviews</p>
           <div className="flex items-center">
             <p className="text-4xl font-semibold">${PriceUseContext.price}</p>
             <p className="pl-3 opacity-70">/ month</p>
           </div>
         </div>
         <Range />
-        <div className="flex items-center justify-end pt-7">
-          <p>Annualy</p>
-          <Toggle />
-          <p>Monthly</p>
+        <div className="flex flex-col md:flex-row items-center justify-end pt-7">
+          <div className="flex items-center pb-5 md:pb-0">
+            <p className="opacity-80">mothly</p>
+            <Toggle />
+            <p className="opacity-80">yearly</p>
+          </div>
           <Badge text="28% discount" />
         </div>
       </div>
       <hr />
       <div className="p-7 flex flex-col md:flex-row items-center justify-between">
         <ul className="mb-5 md:mb-5">
-          <li className="flex items-center py-1">
+          <li className="flex items-center justify-center md:justify-start py-1 ">
             <svg className="w-5 h-5 text-turquoise-200 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" /></svg>
             <p className="opacity-80">option 1</p>
           </li>
-          <li className="flex items-center py-1">
+          <li className="flex items-center justify-center md:justify-start py-1 ">
             <svg className="w-5 h-5 text-turquoise-200 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" /></svg>
             <p className="opacity-80">option 1</p>
           </li>
-          <li className="flex items-center py-1">
+          <li className="flex items-center justify-center md:justify-start py-1 ">
             <svg className="w-5 h-5 text-turquoise-200 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" /></svg>
             <p className="opacity-80">option 1</p>
           </li>
--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -2,14 +2,14 @@ function Toggle() {
   return (
     <label
       htmlFor="toggle"
-      className="flex relative items-center cursor-pointer mx-5"
+      className="flex relative items-ce1nter cursor-pointer mx-5"
     >
       <input
         type="checkbox"
         id="toggle"
         className="sr-only peer"
       />
-      <div className="w-[3.75rem] h-8 bg-turquoise-200 outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-200 after:border after:rounded-full after:h-7 after:w-7 border-gray-600 peer-checked:bg-turquoise-300"></div>
+      <div className="w-[3.5rem] h-8 shadow-md shadow-turquoise-100 bg-turquoise-200 outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[4px] after:left-[4px] after:bg-white after:border-gray-200 after:border after:rounded-full after:h-6 after:w-6 border-gray-600 peer-checked:bg-turquoise-300"></div>
     </label>
   );
 }
